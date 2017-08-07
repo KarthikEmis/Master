7b3c37d55163b584bf7c6767664ed0d3f4662777
Maxim Mylashko <mmylashko@Viktors-Mac-mini.local>
2017-08-07 11:36:26 +0300

    Added constants file

---
+++ PatientAccessWrapper/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  PatientAccessWrapper
+//
+//  Created by Maxim Mylashko on 8/2/17.
+//  Copyright © 2017 uk.co.patient. All rights reserved.
+//
+
+import Foundation
+
+enum APIEndpoints {
+    static let baseURL = "demo266.dataart.com/"
+    
+    static let anchors = "api/api/healthkit/anchors"
+}
